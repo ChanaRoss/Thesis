06d1464b3c34be8669a16ed14e9847946165d38b
chanaby <chanaby@gmail.com>
2019-02-26 22:13:37 +0200

    updated plot analysis + cnn lstm network

--- MachineLearning/plotAnalysisFile.py
+++ MachineLearning/plotAnalysisFile.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
-sys.path.insert(0, '/Users/chanaross/dev/Thesis/MachineLearning/')
-from CNN_LSTM_NeuralNet import Model
+sys.path.insert(0, '/Users/chanaross/dev/Thesis/MachineLearning/forGPU/')
+from CNN_LSTM_NeuralNetOrigV2 import Model
 
 
 # matplotlib.use('Agg')  # allows it to operate on machines without graphical interface
@@ -20,13 +20,13 @@ from CNN_LSTM_NeuralNet import Model
 def save_plots(results,FigName,fileName):
     # plt.clf()
     lgstr = []
-    color_p = ['r','b','m','k','g','y']
-    lineType = ['-','-.']
-    for i in range(0,len(results[0])):
-        plt.plot(range(1,len(results[0][i])+1),results[0][i],color = color_p[i],linewidth = 1.2,linestyle = lineType[0])
-        plt.plot(range(2,len(results[1][i])+2),results[1][i],color = color_p[i+1],linewidth = 1.2,linestyle = lineType[1])
-        lgstr.append('Train - triple Augmentation , ADAM optimizer')
-        lgstr.append('Test - triple Augmentation , ADAM optimizer')
+    color_p = ['r', 'b', 'm', 'k', 'g', 'y']
+    lineType = ['-', '-.']
+    for i in range(0, len(results[0])):
+        plt.plot(range(1, len(results[0][i])+1), results[0][i], color=color_p[i], linewidth=1.2, linestyle=lineType[0])
+        plt.plot(range(2, len(results[1][i])+2), results[1][i], color=color_p[i+1], linewidth=1.2, linestyle=lineType[1])
+        lgstr.append('Train - SGD optimizer')
+        lgstr.append('Test - SGD optimizer')
         # lgstr.append('Train ' + str(i+1))
         # lgstr.append('Test ' + str(i+1))
 
@@ -41,37 +41,40 @@ def save_plots(results,FigName,fileName):
 
 
 
-path = []
+path     = []
 filename = []
 
 
-path.append('/Users/chanaross/dev/Thesis/MachineLearning/')
-filename.append('gridSize20_epoch0_batch84.npy')
+path.append('/Users/chanaross/dev/Thesis/MachineLearning/forGPU/GPU_results/')
+filename.append('/Users/chanaross/dev/Thesis/MachineLearning/forGPU/GPU_results/gridSize21_epoch95_batch4_torch.pkl')
 
-#
-# my_net = torch.load(filename[-1])
-#
-# # my_net = torch.load(filename[-1], map_location=lambda storage, loc: storage)
-#
-# data = np.stack([np.array([]), np.array(my_net.lossVecTrain),
-#                                      np.array([]), np.array(my_net.accVecTrain)])
-epochs = []
-acc_test = []
-acc_train = []
-loss_test = []
-loss_train = []
+
+
 for i in range(0,len(path)):
-    data = np.load(path[i] + filename[i])
+    my_net = torch.load(filename[i], map_location=lambda storage, loc: storage)
+    data = np.stack([np.array(my_net.accVecTrain), np.array(my_net.lossVecTrain), np.array(my_net.rmseVecTrain),
+                     np.array(my_net.accVecTest), np.array(my_net.lossVecTest), np.array(my_net.rmseVecTest)])
+    epochs      = []
+    acc_test    = []
+    acc_train   = []
+    loss_test   = []
+    loss_train  = []
+    rmse_train  = []
+    rmse_test   = []
     epochs.append(range(len(data[0])))
-    loss_test.append(data[0])
+    acc_train.append(data[0])
     loss_train.append(data[1])
-    acc_test.append(data[2])
-    acc_train.append(data[3])
+    rmse_train.append(data[2])
+    acc_test.append(data[3])
+    loss_test.append(data[4])
+    rmse_test.append(data[5])
 
-
-to_plot = [acc_train,acc_test]
-save_plots(to_plot,'AccVsEpoch','Comparison')
+to_plot = [acc_train, acc_test]
+save_plots(to_plot, 'AccVsEpoch', 'Comparison')
 plt.show()
-to_plot = [loss_train,loss_test]
-save_plots(to_plot,'LossVsEpoch','Comparison')
+to_plot = [loss_train, loss_test]
+save_plots(to_plot, 'LossVsEpoch', 'Comparison')
+plt.show()
+to_plot = [rmse_train, rmse_test]
+save_plots(to_plot, 'RmseVsEpoch', 'Comparison')
 plt.show()
