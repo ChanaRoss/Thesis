fixed bug in cost calculation  on network updated plot_mtsp_anticipatory to work with new problem updated offline gurobi calc to multiple depots

MixedIntegerOptimization/offlineOptimizationProblem_unlimited_time_multiple_depots.py
@@ -210,7 +210,7 @@ def run_mtsp_opt(car_pos, event_loc, fake_depot, output_flag=0):
 
     for i in range(n_events):
         for j in range(n_events):
-            if (i != 0) or (j != 0):
+            if (i != 0) and (j != 0):
                 total_cost += distance_matrix[i, j] * x[i, j]
 
     # find the final objective of optimization problem (maximum since we are looking at the rewards)
@@ -364,7 +364,7 @@ def main():
         fake_depot = np.zeros((1, 2))
         # run optimization using gurobi
         s_time = time.time()
-        m, obj = run_mtsp_opt(car_pos, event_pos, fake_depot, True)
+        m, obj = run_mtsp_opt(car_pos, event_pos, fake_depot, False)
         e_time = time.time()
         print("simulation run time is: " + str(e_time - s_time))
         if print_logs:

RL/problems/tsp/problem_tsp.py
@@ -60,10 +60,10 @@ class MTSP(object):
 
     @staticmethod
     def get_costs(dataset, pi):
-        cost = 0
         loc = torch.cat((dataset['car_loc'].clone(), dataset['loc'].clone()), -2)
         pi_assert = pi.permute(1, 0, 2)
         batch_size, tour_length, _ = dataset['loc'].shape
+        cost = torch.zeros(batch_size)
         n_cars = dataset['n_cars'][0].item()
         # Check that tours are valid, i.e. contain 0 to n -1
         # is_full_tour = True
@@ -83,9 +83,10 @@ class MTSP(object):
             dist_mat = calc_distance(d)
             # in order to get the total cost need to sum up the cost of each movement, d is already in the correct order
             # therefore if we sum the [i, i+1] value from each row i and column i+1
-            cost += torch.stack([dist_mat[:, i, i+1] for i in range(dist_mat.shape[1]-1)], dim=1).sum(axis=1)
+            cost1 = torch.stack([dist_mat[:, i, i+1] for i in range(dist_mat.shape[1]-1)], dim=1).sum(axis=1)
             # Add all tour costs together for each car
-            cost += (loc[:, i] - d[:, 0]).norm(p=1, dim=1)  # add the cost of going from car location to the first node
+            cost2 = (loc[:, i] - d[:, 0]).norm(p=1, dim=1)  # add the cost of going from car location to the first node
+            cost += cost1 + cost2
         return cost, None
 
     @staticmethod